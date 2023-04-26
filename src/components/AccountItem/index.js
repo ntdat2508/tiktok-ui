49911bd0599cee746747b97cf67c7e4ea43021fb
Thanh Đạt <ngodat331311@gmail.com>
2023-04-26 22:04:11 +0700

    Xay dung UI phan Header

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -7,64 +7,20 @@ const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
-    <>
-      <div className={cx('wrapper')}>
-        <img
-          className={cx('avatar')}
-          src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1682348400&x-signature=cvCZRRReDdx3M0g8bKhdE4pmqlo%3D"
-          alt="Hoa"
-        />
-        <div className={cx('info')}>
-          <h4 className={cx('name')}>
-            <span>Nguyen Van A</span>
-            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-          </h4>
-          <span className={cx('username')}>nguyenvana</span>
-        </div>
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1682348400&x-signature=cvCZRRReDdx3M0g8bKhdE4pmqlo%3D"
+        alt="Hoa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
       </div>
-      <div className={cx('wrapper')}>
-        <img
-          className={cx('avatar')}
-          src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1682348400&x-signature=cvCZRRReDdx3M0g8bKhdE4pmqlo%3D"
-          alt="Hoa"
-        />
-        <div className={cx('info')}>
-          <h4 className={cx('name')}>
-            <span>Nguyen Van A</span>
-            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-          </h4>
-          <span className={cx('username')}>nguyenvana</span>
-        </div>
-      </div>
-      <div className={cx('wrapper')}>
-        <img
-          className={cx('avatar')}
-          src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1682348400&x-signature=cvCZRRReDdx3M0g8bKhdE4pmqlo%3D"
-          alt="Hoa"
-        />
-        <div className={cx('info')}>
-          <h4 className={cx('name')}>
-            <span>Nguyen Van A</span>
-            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-          </h4>
-          <span className={cx('username')}>nguyenvana</span>
-        </div>
-      </div>
-      <div className={cx('wrapper')}>
-        <img
-          className={cx('avatar')}
-          src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1682348400&x-signature=cvCZRRReDdx3M0g8bKhdE4pmqlo%3D"
-          alt="Hoa"
-        />
-        <div className={cx('info')}>
-          <h4 className={cx('name')}>
-            <span>Nguyen Van A</span>
-            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-          </h4>
-          <span className={cx('username')}>nguyenvana</span>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
 
